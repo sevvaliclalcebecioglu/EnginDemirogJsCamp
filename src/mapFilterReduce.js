9c40a7ca02db0655e273f35021679d5d2e723c96
Şevval İclal Cebecioğlu <161520054+sevvaliclalcebecioglu@users.noreply.github.com>
2024-05-11 19:37:26 +0300

    .

--- src/mapFilterReduce.js
+++ src/mapFilterReduce.js
@@ -1,3 +1,5 @@
+// let cart => array, bu array içindede birden fazla objem olabilir.
+
 let cart = [
     {id:1, productName:"Telefon", quantity:3, unitPrice:4000},
     {id:2, productName:"Bardak", quantity:2, unitPrice:30},
@@ -5,7 +7,10 @@ let cart = [
     {id:4, productName:"Şarj Cihazı", quantity:2, unitPrice:100},
     {id:5, productName:"Kitap", quantity:3, unitPrice:30},
     {id:6, productName:"Pot", quantity:5, unitPrice:150},
-]
+] 
+// Şu an backend sistemimiz olmadığı için manuel bir şekilde yazıyoruz.
+
+// cart.map => mesela sepetin hepsini dolaşmamıza yarar.
 
 console.log("<ul>")
 cart.map(product=>{
@@ -16,7 +21,6 @@ console.log("</ul>")
 
 let total = cart.reduce((acc,product)=>acc+ product.unitPrice * product.quantity,0)
 
-//23.00 Dersteyiz
 console.log(total)
 
 
@@ -24,15 +28,22 @@ let quantityOver2 = cart.filter(product=>product.quantity>2)
 
 console.log(quantityOver2)
 
+
+
+////////////////////////////////////////////////////////
+
+// Angular, react ile geliştirdiğimiz uygulamalara SPA diyoruz.
+// Yani bir datayı değiştirdiğimiz zaman bunu haber vermemiz gerekiyor.
+
 function addToCart(sepet) {
-    sepet.push({id:7, productName:"Ruhsat", quantity:1, unitPrice:20})
+    sepet.push({id:7, productName:"Ruhsat", quantity:1, unitPrice:20}) 
 }
-
-
 
 addToCart(cart)
 
 console.log(cart)
+
+// front-end de referans değiştirmek çok önemli.
 
 let sayi = 10
 
@@ -42,4 +53,3 @@ function sayiTopla(number) {
 sayiTopla(sayi)
 console.log(sayi)
 
-//23.00 Dersteyiz
